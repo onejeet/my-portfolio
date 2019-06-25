17b9344ecd946f85c07a7e36244ede5f6fe61b57
onejeet <jitendra909@gmail.com>
2019-06-25 12:21:00 +0530

    intial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+
+    render(){
+        return (
+            <div>
+                <h1>onejeet</h1>
+            </div>
+        );
+    }
+}
+
+
+export default App;
