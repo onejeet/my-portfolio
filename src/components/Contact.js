8202a174f23349918efbe744bff0a0601a85428b
onejeet <jitendra909@gmail.com>
2019-07-01 17:29:31 +0530

    contact page intial setup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import InfoBox from './InfoBox';
+import ProfilePhoto from './ProfilePhoto';
+import photo from '../assets/j.jpg';
 
 const Contact = () => {
     return (
-        <div className="x">
-            This is Contact Page
+        <div className="contactus">
+            <div className="left-half">
+                <div className="intro">
+                    <p>
+                    You can always drop a message, feedback or query on <a href="mailto:jitendra909@gmail.com">jitendra909@gmail.com</a>. However you can also drop a DM on social media.
+                    </p>
+                </div>
+                
+            </div>
+            <div className="right-half">
+                <ProfilePhoto 
+                width= '140px'
+                photo = {photo}
+                />
+                <InfoBox />
+            </div>
         </div>
     );
 }
