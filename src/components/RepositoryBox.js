ad55708d0d417d07dd74a5cad63f27c91f61676e
onejeet <jitendra909@gmail.com>
2019-07-03 10:48:38 +0530

    language color dot

--- src/components/RepositoryBox.js
+++ src/components/RepositoryBox.js
@@ -4,10 +4,16 @@ function nameFormatter(name){
     let st = name.split('-');
     return st.map((w) => w.charAt(0).toUpperCase()+w.slice(1)).join(' ');
 }
-
+let colors = {
+    'JavaScript':'#f1e05a',
+    'PHP': '#4F5D95',
+    'HTML':'#e34c26',
+    'C':'#555555',
+    'CSS': '#563d7c'
+}
 const RepositoryBox = (props) =>{
     let project = props.project;
-    let color = project.language === 'JavaScript' ? '#ffee55' : '#56b3d6';
+    let color = colors[project.language] ? colors[project.language] : '#000000';
     return (
             <div className="project">
                <h2 className="title"><i className="fa fa-folder" aria-hidden="true"></i><a href={project.html_url} target="_blank" rel="noreferrer noopener">{nameFormatter(project.name)}</a></h2>
