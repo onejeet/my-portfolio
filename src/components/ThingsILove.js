f7175a1572052c067aa89faff4e6235cab3dcd42
onejeet <jitendra909@gmail.com>
2019-07-18 12:48:36 +0530

    small structure and style changes

--- src/components/ThingsILove.js
+++ src/components/ThingsILove.js
@@ -5,18 +5,35 @@ import photography_thumb from '../assets/photography_thumb.jpg';
 class ThingsILove extends PureComponent {
     render(){
         return (
-            <div className="things">
-                    <h1>
-                        Few things I love to do...
-                    </h1>
-                    <div className="list">
-                        <div className="card">
-                            <div className="cover">
-                                <img src={photography_thumb} alt="photography"/>
+            <div className="main">
+                <div className="things">
+                        <h1>
+                            Few things I love to do...
+                        </h1>
+                        <div className="list">
+                            <div className="card">
+                                <div className="cover">
+                                    <img src={photography_thumb} alt="photography"/>
+                                </div>
+                                <div className="title">Photography</div>
                             </div>
-                            <div className="title">Photography</div>
+                            <div className="card">
+                                <div className="cover">
+                                    <img src={photography_thumb} alt="photography"/>
+                                </div>
+                                <div className="title">Video Recording & Editing</div>
+                            </div>
+                            <div className="card">
+                                <div className="cover">
+                                    <img src={photography_thumb} alt="photography"/>
+                                </div>
+                                <div className="title">Mountain Trekkings</div>
+                            </div>
                         </div>
-                    </div>
+                </div>
+                <div className="container">
+    
+                </div>
             </div>
         );
     }
