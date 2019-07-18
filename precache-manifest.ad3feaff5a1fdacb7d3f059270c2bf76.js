14d779b9398064c4c9ec1600f7272ac9adc10ea9
onejeet <jitendra909@gmail.com>
2019-07-18 13:26:13 +0530

    Updates

---
+++ precache-manifest.ad3feaff5a1fdacb7d3f059270c2bf76.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4288d14d9743f53fd80be44a9507651",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "74852912a95d1150d042",
+    "url": "/portfolio/static/css/2.ca1e55e4.chunk.css"
+  },
+  {
+    "revision": "4a773f089e7bd334d371",
+    "url": "/portfolio/static/css/main.cba9b29e.chunk.css"
+  },
+  {
+    "revision": "74852912a95d1150d042",
+    "url": "/portfolio/static/js/2.49637c9c.chunk.js"
+  },
+  {
+    "revision": "4a773f089e7bd334d371",
+    "url": "/portfolio/static/js/main.2b26219c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "87323cf90022fcd8dbdd581c9f634089",
+    "url": "/portfolio/static/media/j.87323cf9.jpg"
+  },
+  {
+    "revision": "2f2ad9512c7ad4ea794d3a5d6adbd69e",
+    "url": "/portfolio/static/media/loader-2.2f2ad951.gif"
+  },
+  {
+    "revision": "4b9182c15d746b98dc576ab5e15a4d5a",
+    "url": "/portfolio/static/media/loader.4b9182c1.gif"
+  },
+  {
+    "revision": "dd7e783272194f45d0d6e8ac65f09601",
+    "url": "/portfolio/static/media/pattern.dd7e7832.png"
+  },
+  {
+    "revision": "2c5352aa9bffae9f0226bdc28a3cbe88",
+    "url": "/portfolio/static/media/photography_thumb.2c5352aa.jpg"
+  },
+  {
+    "revision": "834d2ffb6c2047d9652b0d39b560fa6e",
+    "url": "/portfolio/static/media/trek_thumb.834d2ffb.jpg"
+  },
+  {
+    "revision": "2133a7bcca5561742e32a1943e44f2fc",
+    "url": "/portfolio/static/media/video_thumb.2133a7bc.jpg"
+  }
+]);
